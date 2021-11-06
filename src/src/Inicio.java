f468764e85c6bd522546e43beacf275212b52c08
LEOBAINI <leobaini@hotmail.com>
2021-11-06 18:09:24 -0300

    controles en la carga del input
    Valida que los datos estén ok, para que no haya inconsistencias en el alfabeto/transiciones.
    Falta hacer lo mismo en los estados, que no haya estados sin declarar.

---
+++ src/src/Inicio.java
@@ -0,0 +1,13 @@
+import java.io.FileNotFoundException;
+
+public class Inicio {
+
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+	    GestorAutomata gestor=new GestorAutomata();
+		gestor.leerArchivoInput(".\\settings.txt");
+		gestor.verificarSiEsAFD();
+		gestor.ejecutar("aaaaaaaabaaaaabcbcbcbcbcdbdbdbdbdbcacacacaabcd");
+
+	}
+
+}
